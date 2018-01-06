ac6f2c4d5dbf8ad781c66f8b33e6ed03bb666856
johncaozh <caozhongqian@hotmail.com>
2018-01-06 23:04:31 +0800

    整理资源文件

--- src/frontend/sunrunext/src/main.js
+++ src/frontend/sunrunext/src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import iconFont from '../dist/iconfont.css'
+import iconFont from '../static/iconfont.css'
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
