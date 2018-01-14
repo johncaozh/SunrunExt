268903f6492a05ec2f004ecbd68189b2f23a3900
johncaozh <caozhongqian@hotmail.com>
2018-01-14 13:20:42 +0800

    添加视频模板

--- src/frontend/sunrunext/src/components/common/appMessageTemplateCreator.vue
+++ src/frontend/sunrunext/src/components/common/appMessageTemplateCreator.vue
@@ -76,284 +76,306 @@
     <el-tab-pane>
       <span slot="label" class="label">
         <i class="el-icon-custom-video icon"></i> 视频</span>
-      <div class="flexDiv-v border">
+      <div class="flexDiv-v border" @click="editVideoTemplate">
         <i class="el-icon-plus icon-plus "></i>
         添加视频
       </div>
+      <el-dialog :close-on-click-modal=false title="编辑视频模板" width="800px" :visible.sync="videoMessageContext.isVideoDialogVisible">
+        <app-message-template-creator_video/>
+        <div slot="footer" class="dialog-footer ">
+          <el-button @click="dialogVisible=false" size="small">取消</el-button>
+          <el-button type="primary" size="small" @click="submitForm('urlForm')">提交</el-button>
+        </div>
+      </el-dialog>
     </el-tab-pane>
     <el-tab-pane>
       <span slot="label" class="label">
         <i class="el-icon-custom-file icon"></i> 文件</span>
-        <div>
-          <div v-if="fileMessageContext.mediaId" class="flexDiv-h" style="align-items:flex-end">
-            <div class="flexDiv-h border" style="padding:10px;justify-content:flex-start">
-              <file-icon :file-name="fileMessageContext.fileName" @click="downloadFile"/>
-              <div class="flexDiv-v">
-                <span>{{fileMessageContext.fileName}}</span>
-                <span class="text-font-minor">{{fileMessageContext.fileSize|sizeUnitConverter(fileMessageContext.fileSize)}}</span>
-              </div>
+      <div>
+        <div v-if="fileMessageContext.mediaId" class="flexDiv-h" style="align-items:flex-end">
+          <div class="flexDiv-h border" style="padding:10px;justify-content:flex-start">
+            <file-icon :file-name="fileMessageContext.fileName" @click="downloadFile" />
+            <div class="flexDiv-v">
+              <span>{{fileMessageContext.fileName}}</span>
+              <span class="text-font-minor">{{fileMessageContext.fileSize|sizeUnitConverter(fileMessageContext.fileSize)}}</span>
             </div>
-            <el-button type="text" style="margin-left:10px;" class="button-link" size="small" @click="cleanFileMessageContext">删除</el-button>
           </div>
-          <div class="flexDiv-h" style="align-items:center" v-else>
-            <el-upload class="avatar-uploader" :action="uploadUrl" :show-file-list="false" :on-success="handleFileSuccess" :before-upload="beforeFileUpload"
-              :on-error="handleFileError" v-loading="fileMessageContext.isUploading">
-              <div class="flexDiv-v border">
-                <i class="el-icon-plus icon-plus "></i>
-                添加文件
-              </div>
-            </el-upload>
-            <span class="upload-prompt">文件大小不超过20MB</span>
-          </div>
+          <el-button type="text" style="margin-left:10px;" class="button-link" size="small" @click="cleanFileMessageContext">删除</el-button>
         </div>
+        <div class="flexDiv-h" style="align-items:center" v-else>
+          <el-upload class="avatar-uploader" :action="uploadUrl" :show-file-list="false" :on-success="handleFileSuccess" :before-upload="beforeFileUpload"
+            :on-error="handleFileError" v-loading="fileMessageContext.isUploading">
+            <div class="flexDiv-v border">
+              <i class="el-icon-plus icon-plus "></i>
+              添加文件
+            </div>
+          </el-upload>
+          <span class="upload-prompt">文件大小不超过20MB</span>
+        </div>
+      </div>
     </el-tab-pane>
   </el-tabs>
 </template>
 
 <script>
-import api from "../../utility/api";
-import fileIcon from "./fileIcon.vue";
-export default {
-  data() {
-    return {
-      photoMessageContext: {
-        isUploading: false,
-        imageUrl: null,
-        mediaId: null
-      },
-      voiceMessageContext: {
-        isUploading: false,
-        mediaId: null, //amr媒体ID
-        duration: 0, //秒为单位
-        fileName: null,
-        mediaUrl: null
-      },
-      fileMessageContext: {
-        isUploading: false,
-        mediaId: null,
-        fileSize: 0, //秒为单位
-        fileName: null,
-        mediaUrl: null
-      },
-      uploadUrl: api.fileTransferUrl,
-      uploadUrl_audio: api.fileTransferUrl_audio
-    };
-  },
-
-  components: {
-    fileIcon
-  },
-
-  methods: {
-    handlePhotoSuccess(res, file) {
-      this.photoMessageContext.isUploading = false;
-      this.photoMessageContext.mediaId = res.data;
-      this.photoMessageContext.imageUrl = URL.createObjectURL(file.raw);
+  import api from "../../utility/api";
+  import fileIcon from "./fileIcon.vue";
+  import appMessageTemplateCreator_video from "./appMessageTemplateCreator_video";
+  export default {
+    data() {
+      return {
+        photoMessageContext: {
+          isUploading: false,
+          imageUrl: null,
+          mediaId: null
+        },
+        voiceMessageContext: {
+          isUploading: false,
+          mediaId: null, //amr媒体ID
+          duration: 0, //秒为单位
+          fileName: null,
+          mediaUrl: null
+        },
+        fileMessageContext: {
+          isUploading: false,
+          mediaId: null,
+          fileSize: 0, //秒为单位
+          fileName: null,
+          mediaUrl: null
+        },
+        videoMessageContext: {
+          mediaId: null,
+          thumbMediaId: null,
+          title: null,
+          abstract: null,
+          isVideoDialogVisible: false
+        },
+        uploadUrl: api.fileTransferUrl,
+        uploadUrl_audio: api.fileTransferUrl_audio
+      };
     },
-    beforePhotoUpload(file) {
-      const isvalidPhoto =
-        file.type === "image/jpeg" ||
-        file.type === "image/jpg" ||
-        file.type === "image/png" ||
-        file.type === "image/bmp";
 
-      const isLt2M = file.size / 1024 / 1024 < 2;
-
-      if (!isvalidPhoto) {
-        this.$message.error("上传的图片只能是 JPEG、JPG、PNG、BMP 格式!");
-      } else if (!isLt2M) {
-        this.$message.error("上传的图片大小不能超过 2MB!");
-      }
-
-      var result = isvalidPhoto && isLt2M;
-
-      if (result) this.photoMessageContext.isUploading = true;
-
-      return result;
+    components: {
+      fileIcon,
+      appMessageTemplateCreator_video
     },
-    cleanPhotoMessageContext() {
-      this.photoMessageContext.isUploading = false;
-      this.photoMessageContext.imageUrl = null;
-    },
-    handleVoiceSuccess(res, file) {
-      this.voiceMessageContext.isUploading = false;
-      this.voiceMessageContext.fileName = file.name;
-      this.voiceMessageContext.mediaId = res.data.mediaId;
-      this.voiceMessageContext.duration = res.data.duration;
-      this.voiceMessageContext.mediaUrl = `${api.fileTransferUrl}/${this
+
+    methods: {
+      handlePhotoSuccess(res, file) {
+        this.photoMessageContext.isUploading = false;
+        this.photoMessageContext.mediaId = res.data;
+        this.photoMessageContext.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforePhotoUpload(file) {
+        const isvalidPhoto =
+          file.type === "image/jpeg" ||
+          file.type === "image/jpg" ||
+          file.type === "image/png" ||
+          file.type === "image/bmp";
+
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isvalidPhoto) {
+          this.$message.error("上传的图片只能是 JPEG、JPG、PNG、BMP 格式!");
+        } else if (!isLt2M) {
+          this.$message.error("上传的图片大小不能超过 2MB!");
+        }
+
+        var result = isvalidPhoto && isLt2M;
+
+        if (result) this.photoMessageContext.isUploading = true;
+
+        return result;
+      },
+      cleanPhotoMessageContext() {
+        this.photoMessageContext.isUploading = false;
+        this.photoMessageContext.imageUrl = null;
+      },
+      handleVoiceSuccess(res, file) {
+        this.voiceMessageContext.isUploading = false;
+        this.voiceMessageContext.fileName = file.name;
+        this.voiceMessageContext.mediaId = res.data.mediaId;
+        this.voiceMessageContext.duration = res.data.duration;
+        this.voiceMessageContext.mediaUrl = `${api.fileTransferUrl}/${this
         .voiceMessageContext.mediaId}`;
-    },
-    handleVoiceError(err, file, fileList) {
-      this.voiceMessageContext.isUploading = false;
+      },
+      handleVoiceError(err, file, fileList) {
+        this.voiceMessageContext.isUploading = false;
 
-      if (err.status == 413) this.$message.error("语音时长不能超过一分钟");
-    },
-    beforeVoiceUpload(file) {
-      const isvalidVoice =
-        file.type === "audio/mp3" ||
-        file.type === "audio/wma" ||
-        file.type === "audio/wav" ||
-        file.type === "audio/amr";
+        if (err.status == 413) this.$message.error("语音时长不能超过一分钟");
+      },
+      beforeVoiceUpload(file) {
+        const isvalidVoice =
+          file.type === "audio/mp3" ||
+          file.type === "audio/wma" ||
+          file.type === "audio/wav" ||
+          file.type === "audio/amr";
 
-      const isLt2M = file.size / 1024 / 1024 < 5;
+        const isLt2M = file.size / 1024 / 1024 < 5;
 
-      if (!isvalidVoice) {
-        this.$message.error("上传的语音只能是 MP3、WMA、WAV、AMR 格式!");
-      } else if (!isLt2M) {
-        this.$message.error("上传的语音大小不能超过 5MB!");
-      }
+        if (!isvalidVoice) {
+          this.$message.error("上传的语音只能是 MP3、WMA、WAV、AMR 格式!");
+        } else if (!isLt2M) {
+          this.$message.error("上传的语音大小不能超过 5MB!");
+        }
 
-      var result = isvalidVoice && isLt2M;
+        var result = isvalidVoice && isLt2M;
 
-      if (result) this.voiceMessageContext.isUploading = true;
+        if (result) this.voiceMessageContext.isUploading = true;
 
-      return result;
-    },
-    cleanVoiceMessageContext() {
-      this.voiceMessageContext.mediaId = null;
-      this.voiceMessageContext.isUploading = false;
-    },
-    playVoice() {
-      var audioPlayer = this.$refs.voicePlayer;
+        return result;
+      },
+      cleanVoiceMessageContext() {
+        this.voiceMessageContext.mediaId = null;
+        this.voiceMessageContext.isUploading = false;
+      },
+      playVoice() {
+        var audioPlayer = this.$refs.voicePlayer;
 
-      if (audioPlayer.paused) {
-        audioPlayer.play();
-      } else {
-        audioPlayer.pause();
-      }
-    },
-    handleFileSuccess(res, file) {
-      this.fileMessageContext.isUploading = false;
-      this.fileMessageContext.fileName = file.name;
-      this.fileMessageContext.mediaId = res.data;
-      this.fileMessageContext.fileSize = file.size;
-      this.voiceMessageContext.mediaUrl = `${api.fileTransferUrl}/${this
+        if (audioPlayer.paused) {
+          audioPlayer.play();
+        } else {
+          audioPlayer.pause();
+        }
+      },
+      handleFileSuccess(res, file) {
+        this.fileMessageContext.isUploading = false;
+        this.fileMessageContext.fileName = file.name;
+        this.fileMessageContext.mediaId = res.data;
+        this.fileMessageContext.fileSize = file.size;
+        this.voiceMessageContext.mediaUrl = `${api.fileTransferUrl}/${this
         .fileMessageContext.mediaId}`;
-    },
-    handleFileError(err, file, fileList) {
-      this.voiceMessageContext.isUploading = false;
-      this.$message.error("上传文件出错");
-    },
-    beforeFileUpload(file) {
-      const isLt2M = file.size / 1024 / 1024 < 20;
+      },
+      handleFileError(err, file, fileList) {
+        this.voiceMessageContext.isUploading = false;
+        this.$message.error("上传文件出错");
+      },
+      beforeFileUpload(file) {
+        const isLt2M = file.size / 1024 / 1024 < 20;
 
-      if (!isLt2M) {
-        this.$message.error("上传的文件大小不能超过 20MB!");
+        if (!isLt2M) {
+          this.$message.error("上传的文件大小不能超过 20MB!");
+        }
+
+        if (isLt2M) this.fileMessageContext.isUploading = true;
+        return isLt2M;
+      },
+      cleanFileMessageContext() {
+        this.fileMessageContext.mediaId = null;
+        this.fileMessageContext.isUploading = false;
+      },
+      downloadFile() {
+        var a = document.createElement("a");
+        a.href = this.fileMessageContext.mediaUrl;
+        a.click();
+      },
+      editVideoTemplate() {
+        this.videoMessageContext.isVideoDialogVisible = true;
       }
-
-      if (isLt2M) this.fileMessageContext.isUploading = true;
-      return isLt2M;
-    },
-    cleanFileMessageContext() {
-      this.fileMessageContext.mediaId = null;
-      this.fileMessageContext.isUploading = false;
-    },
-    downloadFile() {
-      var a = document.createElement("a");
-      a.href = this.fileMessageContext.mediaUrl;
-      a.click();
     }
-  }
-};
+  };
+
 </script>
 
 <style lang="less" scoped>
-.el-tabs {
-  padding-left: 15px;
-  padding-right: 15px;
-  padding-bottom: 15px;
-  background: #fbfbfb;
-  min-height: 300px;
-}
+  .el-tabs {
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-bottom: 15px;
+    background: #fbfbfb;
+    min-height: 300px;
+  }
 
-.icon {
-  font-size: 14px;
-  color: @color-font-minor;
-}
+  .icon {
+    font-size: 14px;
+    color: @color-font-minor;
+  }
 
-.icon-plus {
-  font-size: 32px;
-  color: @color-theme;
-}
+  .icon-plus {
+    font-size: 32px;
+    color: @color-theme;
+  }
 
-.icon-voiceMessage {
-  font-size: 20px;
-  color: @color-theme;
-}
+  .icon-voiceMessage {
+    font-size: 20px;
+    color: @color-theme;
+  }
 
-.label {
-  font-size: 14px;
-  color: @color-font-minor;
-}
+  .label {
+    font-size: 14px;
+    color: @color-font-minor;
+  }
 
-textarea {
-  border: 0px;
-  background: transparent;
-  font-size: 14px;
-  resize: none;
-  width: 100%;
-  height: 100%;
-  height: 240px;
-  word-break: break-all;
-  color: @color-font-minor;
-  outline: none;
-  overflow-x: hidden;
-  overflow-y: auto;
-}
+  textarea {
+    border: 0px;
+    background: transparent;
+    font-size: 14px;
+    resize: none;
+    width: 100%;
+    height: 100%;
+    height: 240px;
+    word-break: break-all;
+    color: @color-font-minor;
+    outline: none;
+    overflow-x: hidden;
+    overflow-y: auto;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  }
 
-textarea::-webkit-input-placeholder {
-  color: @color-font-placeholder;
-}
+  textarea::-webkit-input-placeholder {
+    color: @color-font-placeholder;
+  }
 
-.border {
-  float: left;
-  border: 1px solid @color-border-level2;
-  border-radius: 5px;
-  padding: 95px;
-  font-size: 14px;
-  background: white;
-  color: @color-theme;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-}
+  .border {
+    float: left;
+    border: 1px solid @color-border-level2;
+    border-radius: 5px;
+    padding: 95px;
+    font-size: 14px;
+    background: white;
+    color: @color-theme;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
 
-.border:hover {
-  border: 1px solid @color-theme;
-}
+  .border:hover {
+    border: 1px solid @color-theme;
+  }
 
-.upload-prompt {
-  margin-left: 10px;
-  max-width: 300px;
-  word-wrap: break-word;
-  color: @color-font-minor;
-  font-size: 12px;
-}
+  .upload-prompt {
+    margin-left: 10px;
+    max-width: 300px;
+    word-wrap: break-word;
+    color: @color-font-minor;
+    font-size: 12px;
+  }
 
-.avatar-uploader .el-upload {
-  border: 1px solid @color-border-level2;
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-}
+  .avatar-uploader .el-upload {
+    border: 1px solid @color-border-level2;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
 
-.avatar-uploader .el-upload:hover {
-  border-color: #409eff;
-}
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
 
-.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
-  text-align: center;
-}
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
 
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
-}
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+
 </style>
---
+++ src/frontend/sunrunext/src/components/common/appMessageTemplateCreator_video.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="flexDiv-h " style="align-items:flex-start">
+    <div class="flexDiv-v editContainer" style="flex:1">
+      <div class="editItemContainer" style="padding-top:0px">
+        <input type="text" v-model="title" placeholder="在此输入标题" class="input-title" />
+      </div>
+      <div class="editItemContainer">
+        <span class="text-font-minor">
+          <el-button type="text" size="small" class="button-link" style="margin-right:10px;">添加视频</el-button>
+          不超过20MB, 文件格式: rm, rmvb, wmv, avi, mpg, mpeg, mp4
+        </span>
+      </div>
+      <div class="editItemContainer">
+        <div class="flexDiv-h">
+          摘要：
+          <textarea v-model="abstract" placeholder="选填" />
+        </div>
+      </div>
+    </div>
+    <div class="flexDiv-v previewContainer">
+      <div class="text-font-normal preview-title" style="line-height:18px">
+        {{title}}
+      </div>
+      <div class="preview-media">
+        <img>
+      </div>
+      <div class="text-font-minor preview-abstract" style="line-height:16px">
+        {{abstract}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        mediaId: null,
+        thumbMediaId: null,
+        title: null,
+        abstract: null
+      };
+    }
+  };
+
+</script>
+
+<style lang="less" scoped>
+  .editContainer {}
+
+  .editItemContainer {
+    border-bottom: 1px solid @color-border-level2;
+    padding-top: 30px;
+    padding-bottom: 30px;
+  }
+
+  .previewContainer {
+    border: 1px solid @color-border-level2;
+    padding: 10px;
+    width: 250px;
+    min-height: 125px;
+    margin-left: 20px;
+  }
+
+  .input-title {
+    border: 0px;
+    color: @color-theme;
+    outline: none;
+    overflow-x: hidden;
+    overflow-y: auto;
+    font-size: 23px;
+    width: 100%;
+  }
+
+  .input-title::-webkit-input-placeholder {
+    color: @color-theme;
+  }
+
+  textarea {
+    border: 0px;
+    background: transparent;
+    resize: none;
+    outline: none;
+    overflow-x: hidden;
+    overflow-y: auto;
+    flex: 1;
+    height: 60px;
+    word-wrap: break-word;
+    word-break: break-all;
+    padding-top: 3px;
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  }
+
+  textarea::-webkit-input-placeholder {
+    color: @color-font-placeholder;
+  }
+
+  .preview-title {
+    font-size: 16px;
+    word-wrap: break-word;
+  }
+
+  .preview-abstract {
+    word-wrap: break-word;
+  }
+
+  .preview-media{ 
+    background: @color-border-level2;
+    min-height: 120px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+</style>
