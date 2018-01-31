a5625f1bbf77d1b958061f72b1feb91ae8b2d301
johncao <caozhongqian@hotmail.com>
2018-01-31 09:43:27 +0800

    修复上传问题

--- src/frontend/sunrunext/src/components/common/securityManage.vue
+++ src/frontend/sunrunext/src/components/common/securityManage.vue
@@ -27,23 +27,6 @@ export default {
       await api.getConfig();
     }
   },
-  handleAvatarSuccess(res, file) {
-    this.logoUrl = `${api.fileTransferUrl}/${res.data}`;
-    this.config.enterpriseLogoMediaId = res.data;
-  },
-  beforeAvatarUpload(file) {
-    const isJPG = file.type === "image/jpeg";
-    const isLt2M = file.size / 1024 / 1024 < 2;
-
-    if (!isJPG) {
-      this.$message.error("上传头像图片只能是 JPG 格式!");
-    }
-    if (!isLt2M) {
-      this.$message.error("上传头像图片大小不能超过 2MB!");
-    }
-
-    return isJPG && isLt2M;
-  }
 };
 </script>
 <style lang="less" scoped>
